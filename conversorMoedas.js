33b62ea9e9d9177308386ba2c1bf87fb7606fe59
duduetc <eduardo.etca@gmail.com>
2024-04-11 09:41:07 -0300

    mudanças 11.04

--- conversorMoedas.js
+++ conversorMoedas.js
@@ -1,37 +1,40 @@
 let valoresConversao = {
     dolar: {
         euro: 0.92,
-        libra: 0.79,
+        /* libra: 0.79, */
         real: 5.02
     },
 
     euro: {
         dolar: 1.09,
-        libra: 0.86,
+        /* libra: 0.86, */
         real: 5.46
     },
 
-    franco: {
+    /* franco: {
         dolar: 1.11,
         euro: 1.02,
-        libra: 0.87,
+        libra: 0.87, 
         real: 5.57
-    },
+    }, */
 
-    libra: {
+    /* libra: {
         dolar: 1.27,
         euro: 1.17,
         real: 6.36
-    },
+    }, */
 
     real: {
         dolar: 0.20,
         euro: 0.18,
-        libra: 0.16
+        /* libra: 0.16 */
     }
 };
 
-
+let valorUsuario = document.getElementById("valor-usuario");
+valorUsuario.addEventListener("keypress", function(event) {
+    
+});
 
 function converter() {
     let valorUsuario = document.getElementById("valor-usuario").value;
@@ -41,8 +44,24 @@ function converter() {
 
     let conversao = valorUsuario * valoresConversao[moedaOrigem][moedaDestino];
 
+    if(moedaOrigem == moedaDestino){
+        alert("As moedas são iguais, não é possível converter");
+        return;
+    }
+
+    let simbolo = "";
+    if(moedaDestino == "real"){
+        simbolo = "R$";
+    }
+    if(moedaDestino == "dolar"){
+        simbolo = "US$"
+    }
+    if(moedaDestino == "euro"){
+        simbolo = "€";
+    }
+
     let paragrafoResultado = document.getElementById("resultado");
-    paragrafoResultado.textContent = conversao;
+    paragrafoResultado.textContent = simbolo + " " + conversao.toFixed(2);
 
     console.log(conversao);
 }
@@ -53,4 +72,12 @@ function inverter() {
     
     document.getElementById("moeda1").value = moeda2;
     document.getElementById("moeda2").value = moeda1;
+}
+
+function limpar() {
+    let valorUsuario = document.getElementById("valor-usuario");
+    let resultado = document.getElementById("resultado");
+
+    valorUsuario.value = "";
+    resultado.textContent = "";
 }
